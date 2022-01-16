8ff214fedfabdd953ef4bd8c1f689347ae2177d8
PronDmytro <dmytropron5838@gmail.com>
2022-01-17 01:23:31 +0200

    feat: make login page

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../core/services/auth/auth.service';
+import { DataService } from '../../../services/data.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  public form: FormGroup;
 
-  ngOnInit(): void {
+  public constructor(
+    private readonly fb: FormBuilder,
+    private readonly authService: AuthService,
+    private readonly dataService: DataService,
+  ) {
+    this.form = fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  public async login() {
+    if (this.form.valid) {
+      await this.authService.auth(this.form.get('username')!.value, this.form.get('password')!.value);
+      this.dataService.username = this.form.get('username')!.value;
+    } else {
+      this.form.markAllAsTouched();
+    }
   }
 
 }
