3ebd61118553dc230de57fa70897642f02492ebf
PronDmytro <dmytropron5838@gmail.com>
2022-01-17 01:12:18 +0200

    feat: create custom validator for compare fields

---
+++ src/app/core/services/vatidators/custom-validation.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+@Injectable({
+  providedIn: 'root',
+})
+export class CustomValidationService {
+
+  public mustMatch(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ mustMatch: true });
+      } else {
+        matchingControl.setErrors(null);
+      }
+    };
+  }
+
+}
