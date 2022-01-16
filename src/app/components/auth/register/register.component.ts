76ab24d359b61f58452ec785a3622b1a01453ad0
PronDmytro <dmytropron5838@gmail.com>
2022-01-17 01:23:42 +0200

    feat: make register page

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../core/services/auth/auth.service';
+import { CustomValidationService } from '../../../core/services/vatidators/custom-validation.service';
+import { DataService } from '../../../services/data.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  public form: FormGroup;
+  public submitted = false;
 
-  ngOnInit(): void {
+  public constructor(
+    private readonly fb: FormBuilder,
+    private readonly authService: AuthService,
+    private readonly dataService: DataService,
+    private customValidator: CustomValidationService,
+  ) {
+    this.form = fb.group(
+      {
+        username: ['', [Validators.required]],
+        password: ['', [Validators.required]],
+        confirmPassword: ['', [Validators.required]],
+        acceptTerms: [false, Validators.requiredTrue],
+      },
+      { validators: this.customValidator.mustMatch('password', 'confirmPassword') });
+  }
+
+  public async register() {
+    this.submitted = true;
+    if (this.form.valid) {
+      await this.authService.register({
+        username: this.form.get('username')!.value,
+        password: this.form.get('password')!.value,
+      });
+
+      this.dataService.username = this.form.get('username')!.value;
+    } else {
+      this.form.markAllAsTouched();
+    }
   }
 
 }
