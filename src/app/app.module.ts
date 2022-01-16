641f56fd75c3c6a6bd33004efffe584c8fda3034
PronDmytro <dmytropron5838@gmail.com>
2022-01-16 14:32:38 +0200

    feat: create main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { FrameComponent } from './components/frame/frame.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { ProductListComponent } from './components/profile/product-list/product-list.component';
+import { ProductComponent } from './components/profile/product/product.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { HttpTokenInterceptor } from './core/interceptors/http.token.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
+    FrameComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProductListComponent,
+    ProductComponent,
   ],
   imports: [
     BrowserModule,
