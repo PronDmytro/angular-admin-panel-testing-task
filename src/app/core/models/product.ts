74c687c1f3bbb8631048c42fb827a90c7ec98d69
PronDmytro <dmytropron5838@gmail.com>
2022-01-17 01:10:59 +0200

    fix: fix models

--- src/app/core/models/product.ts
+++ src/app/core/models/product.ts
@@ -1,6 +1,6 @@
 export interface Product {
   id: number
   title: string
-  image: string
+  img: string
   text: string
 }
--- src/app/core/models/review.ts
+++ src/app/core/models/review.ts
@@ -2,7 +2,8 @@ import { User } from './user';
 
 export interface Review {
   id: number
-  rate: string
+  rate: number
+  text: string
   product: number
   created_by: User
   created_at: Date
