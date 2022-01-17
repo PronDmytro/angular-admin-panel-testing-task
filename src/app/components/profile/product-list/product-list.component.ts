d4fbc70eb83ce9f9745b49ef81b072e4efd559ab
PronDmytro <dmytropron5838@gmail.com>
2022-01-17 03:36:23 +0200

    feat: make product components

--- src/app/components/profile/product-list/product-list.component.ts
+++ src/app/components/profile/product-list/product-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../../../core/models/product';
+import { UserApiService } from '../../../services/user-api.service';
+import { Router } from '@angular/router';
+import { DataService } from '../../../services/data.service';
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  public isLoading = true;
+  public productList: Product[] | undefined;
 
-  ngOnInit(): void {
+  public constructor(
+    private readonly userApiService: UserApiService,
+    private router: Router,
+    private readonly dataService: DataService,
+  ) {
+  }
+
+  public async ngOnInit(): Promise<void> {
+    this.productList = await this.userApiService.getProducts();
+    this.isLoading = false;
+  }
+
+  public async openProduct(product: Product) {
+    this.dataService.selectedProduct = product;
+    await this.router.navigate(['account/product']);
   }
 
 }
--- src/app/components/profile/product/product.component.ts
+++ src/app/components/profile/product/product.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserApiService } from '../../../services/user-api.service';
+import { DataService } from '../../../services/data.service';
+import { Product } from '../../../core/models/product';
+import { Review } from '../../../core/models/review';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +13,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  public product: Product | undefined;
+  public reviews: Review[] | undefined;
+  public reviewForm: FormGroup;
 
-  ngOnInit(): void {
+  public constructor(
+    private readonly fb: FormBuilder,
+    private readonly userApiService: UserApiService,
+    private readonly dataService: DataService,
+    private router: Router,
+  ) {
+    this.reviewForm = fb.group({
+      review: ['', [Validators.required]],
+      rating: ['', [Validators.required]],
+    });
+  }
+
+  public async ngOnInit(): Promise<void> {
+    this.product = this.dataService.selectedProduct;
+    if (this.product) {
+      this.reviews = await this.userApiService.getReviews(this.product.id);
+    } else {
+      await this.router.navigate(['account/']);
+    }
+  }
+
+  public async submit(): Promise<void> {
+    if (this.reviewForm.value && this.product) {
+      const res = await this.userApiService.createReview(this.product!.id, {
+        text: this.reviewForm.get('review')!.value,
+        rate: Number(this.reviewForm.get('rating')!.value),
+      });
+      console.log(res);
+      this.reviews = await this.userApiService.getReviews(this.product.id);
+    }
   }
 
 }
